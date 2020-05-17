d43554e6ed4bcec1123a0445403a8d23663a2cfe
Edson Cepeda <raulcepedac@hotmail.com>
2020-05-16 23:28:28 -0500

    Completed convolution, pooling and relu layers

--- Convolutions/XOsConvNet/Pooling.py
+++ Convolutions/XOsConvNet/Pooling.py
@@ -11,19 +11,36 @@ test = np.array([
     [0.33,-0.11,0.55,0.33,0.11,-0.11,0.77]
 ])
 
+test2 = np.array([
+    [1,0.3,0.6,0.3],
+    [0.3,1,0.3,0.6],
+    [0.6,0.3,1,0.1],
+    [0.3,0.6,0.1,0.8]
+])
+
+# for i in range(test.shape[0]):
+#     for j in range(test.shape[1]):
+#         ax[-1].text(j,i,test[i,j], ha="center", va="center", color="red")
+
 def max_pooling(matrix, size, stride):
-    pooled_matrix = np.zeros((size * 2, size * 2))
+    h,w = matrix.shape
+    if h%2!=0:
+        h=h+1
+    if w%2!=0:
+        w=w+1
+    pooled_matrix = np.zeros((int(h/2),int(w/2)))
     movey = 0
     for i in range(pooled_matrix.shape[0]):
         movex = 0
         for j in range(pooled_matrix.shape[1]):
-            pooled_matrix[i,j] = (matrix[movey:movey+stride,movex:movex+stride]).max()
-            movex += 2
-        movey += 2
+            pooled_matrix[i,j] = (matrix[movey:movey+size,movex:movex+size]).max()
+            movex += stride
+        movey += stride
     return pooled_matrix
     
 
-#print(max_pooling(test,2,2))
+# test = max_pooling(test2,2,2)
+# print(test)
 # plt.imshow(test,cmap="gray")
 # plt.show()
 
--- Convolutions/XOsConvNet/XOsConvNet.py
+++ Convolutions/XOsConvNet/XOsConvNet.py
@@ -4,60 +4,71 @@ import cv2 as cv
 import Kernels
 import Convolutions
 import Pooling
+import RELU
+
+def sigmoid(x):
+    return(1/(1 + np.exp(-x)))
+    
+def multiPlotting(img_array, img_names):
+    fig = plt.figure(figsize=(18,len(img_array)))
+    ax = []
+    for i in range(len(img_array)):
+        ax.append(fig.add_subplot(1,len(img_array),i+1))
+        ax[-1].set_title(img_names[i])
+        for j in range(img_array[i].shape[0]):
+            for k in range(img_array[i].shape[1]):
+                ax[-1].text(k,j, "{:0.1f}".format(img_array[i][j,k]), ha="center", va="center",color="orange")
+        plt.imshow(img_array[i],cmap="gray")
+    fig.tight_layout()
+    plt.show()
+    
 
 class ConvNet():
     def __init__(self,layers):
         self.layers = layers
     
-    def train(self,inputImage):
-        #Normalize between -1 and 1
-        inputImage = inputImage/255
-        inputImage[inputImage <= 0] = -1
-        
-        for i in self.layers:
-            if i == "Convolution":
-                #Apply kernel
-                inputImage = Convolutions.crossCorr2d(inputImage, Kernels.diagonal_left, padding=True)
-            elif i == "Pooling":
-                inputImage = Pooling.max_pooling(inputImage,1,1)
+    def process(self,inputImage):
+        original_image = inputImage.copy()
+        process_sequence = []
+        img_names = []
+        neurons_results = []
+        for j in self.layers:
+            inputImage = original_image.copy()
+            for i in j:
+                if i[0] == "Convolution":
+                    inputImage = Convolutions.crossCorr2d(inputImage, i[1], padding=i[2])
+                    # process_sequence.append(inputImage)
+                    # img_names.append(i[0])
+                elif i[0] == "Pooling":
+                    inputImage = Pooling.max_pooling(inputImage,i[1],i[2])
+                    # process_sequence.append(inputImage)
+                    # img_names.append(i[0])
+                elif i[0] == "RELU":
+                    inputImage = RELU.RELU(inputImage)
+                    # process_sequence.append(inputImage)
+                    # img_names.append(i[0])
+            # multiPlotting(process_sequence,img_names)
+            neurons_results.append(inputImage)
+        return np.array(neurons_results)
+                 
+XosNet = ConvNet(
+    [
+        [["Convolution",Kernels.diagonal_left,False], ["RELU"], ["Pooling",2,2], ["Pooling",2,2]],
+        [["Convolution",Kernels.cross,False], ["RELU"], ["Pooling",2,2], ["Pooling",2,2]],
+        [["Convolution",Kernels.diagonal_right,False], ["RELU"], ["Pooling",2,2], ["Pooling",2,2]]
+    ]
+)
+         
+#Intialize weights and biases
+weights = np.random.rand(12,2)  
+bias = np.random.rand(1,2)
+
+#Data
+images = np.array(["X.png","X_S.png", "X_G.png", "X_A.png", "C.png", "C_S.png", "C_A.png", "C_G.png"])   
+correct_result = np.array([[1,0],[1,0],[1,0],[1,0],[0,1],[0,1],[0,1],[0,1]])
             
-        return inputImage
-    
-            
-
-original_image = cv.imread('X.png',0)
-#Normalize between -1 and 1
-original_image = (original_image/255)
-original_image[original_image <= 0] = -1
-
-#Apply kernel
-filtered_image = Convolutions.crossCorr2d(original_image, Kernels.diagonal_left, padding=True)
-#Pool
-pooled_image = Pooling.max_pooling(filtered_image,2,2)
-
-filtered_image = Convolutions.crossCorr2d(filtered_image, Kernels.diagonal_left, padding=True)
-pooled_image = Pooling.max_pooling(filtered_image,1,1)
-
-XosNet = ConvNet(["Convolution", "Pooling", "Convolution", "Pooling"])
-print(XosNet.layers)
-
-fig = plt.figure(figsize=(1,3))
-ax = []
-ax.append(fig.add_subplot(1,4,1))
-ax[-1].set_title("Original image")
-plt.imshow(original_image, cmap="gray")
-
-ax.append(fig.add_subplot(1,4,2))
-ax[-1].set_title("Filtered image")
-plt.imshow(filtered_image, cmap="gray")
-
-ax.append(fig.add_subplot(1,4,3))
-ax[-1].set_title("Pooled image")
-plt.imshow(pooled_image, cmap="gray")
-
-ax.append(fig.add_subplot(1,4,4))
-ax[-1].set_title("Network output")
-plt.imshow(XosNet.train(original_image), cmap="gray")
-
-plt.show()
-
+# test_image = cv.imread("X.png",0)
+# test_image = test_image/255
+# test_image[test_image <= 0] = -1
+# result_convs = XosNet.process(test_image).reshape(1,12)
+# print(sigmoid(np.dot(result_convs,weights) + bias))
