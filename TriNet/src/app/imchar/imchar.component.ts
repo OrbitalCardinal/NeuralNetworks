3a491935b0675401b328132cd879f8d6bdddcdcf
Edson Cepeda <raulcepedac@hotmail.com>
2020-05-20 01:34:19 -0500

    Added p5js canvas

--- TriNet/src/app/imchar/imchar.component.ts
+++ TriNet/src/app/imchar/imchar.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import p5 from "p5";
 @Component({
     selector: 'imchar',
     templateUrl: './imchar.component.html',
     styleUrls: ['./imchar.component.css']
 })
   
-export class ImcharComponent {}
+export class ImcharComponent implements OnInit {
+    @ViewChild("canvas") public canvas: ElementRef;
+    ngOnInit() {
+        const sketch = s => {
+            s.setup = () => {
+                let canvas2 = s.createCanvas(200,200);
+                canvas2.parent("sketch");
+                s.background(0);
+            };
+            s.draw = () => {
+                
+            };
+        };
+        this.canvas = new p5(sketch);
+    }
+}
