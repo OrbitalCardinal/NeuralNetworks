df89c1c74abfa9b6888ba8754a4a502d06798c2d
zanais <sony_trevi@hotmail.com>
2020-05-21 16:52:14 -0500

    pixelart siono

--- TriNet/src/app/imchar/imchar.component.ts
+++ TriNet/src/app/imchar/imchar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import p5 from "p5";
+import { getLocaleTimeFormat } from '@angular/common';
 @Component({
     selector: 'imchar',
     templateUrl: './imchar.component.html',
@@ -9,15 +10,42 @@ import p5 from "p5";
 export class ImcharComponent implements OnInit {
     @ViewChild("canvas") public canvas: ElementRef;
     ngOnInit() {
+        let sizeheight=200;
+        let sizewidth=200;
         const sketch = s => {
             s.setup = () => {
                 let canvas2 = s.createCanvas(400,400);
                 canvas2.parent("sketch");
-                s.background(0);
             };
             s.draw = () => {
-                
-            };
+                s.background(220);
+                for(s.y=0; s.y<sizeheight; s.y++){
+                    for(s.x=0; s.x<sizewidth; s.x++){
+                        if(s.getItem(s.x+":"+s.y)==null){
+                            s.storeItem(s.x +":"+ s.y, false)
+                        }
+                        if(s.getItem(s.x+":"+s.y)==true){
+                            s.fill(0);
+                        }
+                        s.rect(s.x*30, s.y*30, 30, 30)
+                            s.noFill();
+                    }
+                }
+            }
+            s.mouseClicked = () =>{
+                for(s.y=0; s.y<sizeheight; s.y++){
+                    for(s.x=0; s.x<sizewidth; s.x++){
+                        if(s.mouseX < s.x*30+30 && s.mouseX > s.x*30 && s.mouseY < s.y*30+30 && s.mouseY > s.y*30){
+                            s.storeItem(s.x+":"+s.y, true)
+                        }
+                    }
+                }
+            }
+            s.keyTyped = () =>{
+                if(s.key == " "){
+                    s.clearStorage()
+                }
+            }
         };
         this.canvas = new p5(sketch);
     }
