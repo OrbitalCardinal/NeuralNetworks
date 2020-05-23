462c29c13f12271a7b425545b39f1a4ac722b43e
Edson Cepeda <raulcepedac@hotmail.com>
2020-05-22 22:22:13 -0500

    Started backend, route /api/imchar for testing

---
+++ TriNet/backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Header","Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    next();
+});
+
+app.use("/api/imchar",(req,res,next) => {
+    res.status(200).json({
+        message:  "Prueba"
+    });
+});
+
+module.exports = app;
